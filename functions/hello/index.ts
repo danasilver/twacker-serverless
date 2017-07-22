9e40ba080dc0205fbdbb016ac8693c8551b85c5c
Dana Silver <dsilver1221@gmail.com>
2017-07-21 21:55:34 -0700

    Use @types/aws-lambda.

--- functions/hello/index.ts
+++ functions/hello/index.ts
@@ -1,6 +1,8 @@
+import { Context, Callback } from 'aws-lambda';
+
 console.log('starting function');
 
-export function handle(e: Object, ctx: Object, cb: Function) {
+export function handle(e: any, ctx: Context, cb: Callback) {
     console.log('processing event %j', e);
     cb(null, { hello: 'world' });
 }
